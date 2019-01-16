9fd60bb879611cd3ddf97bca0135893081c59122
Kirill Mesnyankin <sadbox.games@gmail.com>
2019-01-16 11:36:14 +0300

    Fix typo in file path

--- immutable/index.js
+++ immutable/index.js
@@ -3,5 +3,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('../dist/structure-ops.immutable.cjs');
 } else {
-  module.exports = require('./dist/structure-ops.immutable.develop.cjs');
+  module.exports = require('../dist/structure-ops.immutable.develop.cjs');
 }
